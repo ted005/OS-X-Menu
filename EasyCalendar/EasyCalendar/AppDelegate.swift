ca79db8ebe73f3fef658076c54f726f91ed264c4
ted005 <witwiky@me.com>
2015-05-07 08:31:33 +0800

    initial version of OS X app

---
+++ EasyCalendar/EasyCalendar/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  EasyCalendar
+//
+//  Created by Robbie on 15/5/6.
+//  Copyright (c) 2015年 Ted Wei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+    
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("printQuote:")
+        }
+        
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Print Quote", action: Selector("printQuote:"), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separatorItem())
+        menu.addItem(NSMenuItem(title: "Quit Quotes", action: Selector("terminate:"), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
+    
+    //selector
+    func printQuote(sender: AnyObject) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        println("\(quoteText) — \(quoteAuthor)")
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EasyCalendar/EasyCalendar/TWCalendarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TWCalendarViewController.swift
+//  EasyCalendar
+//
+//  Created by Robbie on 15/5/6.
+//  Copyright (c) 2015年 Ted Wei. All rights reserved.
+//
+
+import Cocoa
+
+class TWCalendarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
